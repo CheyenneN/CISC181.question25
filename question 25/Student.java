b5614f51d5f3a9bfb804086a311bd5990a4c1662
Cheyenne <Cheyenne@128.4.54.213>
2014-09-28 20:40:31 -0400

    Question 25 code. only needed student. did person to have student x's go away.

---
+++ question 25/Student.java
@@ -0,0 +1,13 @@
+
+public class Student extends Person {
+
+	private String CLASS_STATUS;
+	
+	public void student(String CLASS_STATUS){
+	this.CLASS_STATUS = CLASS_STATUS;
+	}
+	public String getClassStatus(){
+		return CLASS_STATUS;
+	}
+}
+
